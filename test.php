9b251a45ab3a454442e2ea0df64509100c4788d3
Mordecai105 <46405782+Mordecai105@users.noreply.github.com>
2020-10-25 17:06:56 -0600

    Update test.php

--- test.php
+++ test.php
@@ -5,13 +5,15 @@
     </head>
 
     <body>
-        <form class="contact-form" action="contactform.php" method="post">
-            <input type="text" name="name" placeholder="Full name" />
-            <input type="text" name="mail" placeholder="Your Email" />
-            <input type="text" name="subject" placeholder="Subject" />
-            <textarea name="message" placeholder="Message"></textarea>
-            <button type="submit" name="submit">Send Mail</button>
+		<main>
+			<form class="contact-form" action="contactform.php" method="post">
+				<input type="text" name="name" placeholder="Full name" />
+				<input type="text" name="mail" placeholder="Your Email" />
+				<input type="text" name="subject" placeholder="Subject" />
+				<textarea name="message" placeholder="Message"></textarea>
+				<button type="submit" name="submit">Send Mail</button>
 
-        </form>
+			</form>
+		 </main>
     </body>
 </html>
